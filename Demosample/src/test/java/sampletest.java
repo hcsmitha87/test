66a1065c58f6e95125f21218e4a617fb8d2130d8
shwet <shwet@Shritha.fritz.box>
2025-09-18 12:40:53 +0200

    commit

---
+++ Demosample/src/test/java/sampletest.java
@@ -0,0 +1,13 @@
+
+public class sampletest {
+
+	public static void main(String[] args) {
+		System.out.println("hello github");
+		System.out.println("hello github");
+		System.out.println("hello github");
+		System.out.println("hello github");
+		System.out.println("hello github");
+
+	}
+
+}
