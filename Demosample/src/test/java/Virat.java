96084fe4f79794a75e9c561075ce2f2378028561
shwet <shwet@Shritha.fritz.box>
2025-09-19 11:01:14 +0200

    Virat changes

---
+++ Demosample/src/test/java/Virat.java
@@ -0,0 +1,13 @@
+
+public class Virat {
+
+	public static void main(String[] args) {
+		System.out.println("hello Virat");
+		System.out.println("hello Virat");
+		System.out.println("hello Virat");
+		System.out.println("hello Virat");
+		System.out.println("hello Virat");
+
+	}
+
+}
